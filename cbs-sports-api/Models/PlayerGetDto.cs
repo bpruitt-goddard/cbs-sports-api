f339af5195fe9440866b60d88e0d31311c729759
Brian Pruitt-Goddard <bpruitt-goddard@github>
2023-10-25 22:26:00 -0700

    fix: update json response of get player
    to snake_case

--- cbs-sports-api/Models/PlayerGetDto.cs
+++ cbs-sports-api/Models/PlayerGetDto.cs
@@ -1,12 +1,33 @@
+using System.Text.Json.Serialization;
+
 namespace cbs_sports_api.Models;
 
-public record PlayerGetDto(int Id, string FirstName, string LastName, string Position, int? Age, SportEnum Sport, double AveragePositionAgeDiff)
+public record PlayerGetDto
 {
+	public int Id { get; init; }
+	[JsonPropertyName("first_name")]
+	public string FirstName { get; init; }
+	[JsonPropertyName("last_name")]
+	public string LastName { get; init; }
+	public string Position { get; init; }
+	public int? Age { get; init; }
+	[JsonIgnore]
+	public SportEnum Sport { get; init; }
+	[JsonPropertyName("average_position_age_diff")]
+	public double AveragePositionAgeDiff { get; init; }
+
 	public PlayerGetDto(Player player, double averagePositionAgeDiff)
-	  : this(player.Id, player.FirstName, player.LastName, player.Position, player.Age, player.Sport, averagePositionAgeDiff)
 	{
+		Id = player.Id;
+		FirstName = player.FirstName;
+		LastName = player.LastName;
+		Position = player.Position;
+		Age = player.Age;
+		Sport = player.Sport;
+		AveragePositionAgeDiff = averagePositionAgeDiff;
 	}
 
+	[JsonPropertyName("name_brief")]
 	public string NameBrief {
 		get => Sport switch
 		{
@@ -16,5 +37,5 @@ public record PlayerGetDto(int Id, string FirstName, string LastName, string Pos
 		};
 	}
 
-	public static string InitialFromString(string s) => s.Substring(0,1);
+	private static string InitialFromString(string s) => s.Substring(0,1);
 }	
