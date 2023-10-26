ec7b043bed75254d22f61efaaeac9456d73f1ff2
Brian Pruitt-Goddard <bpruitt-goddard@github>
2023-10-25 20:58:07 -0700

    feat: add player namebrief property

--- cbs-sports-api/Models/Player.cs
+++ cbs-sports-api/Models/Player.cs
@@ -3,4 +3,16 @@ using Microsoft.EntityFrameworkCore;
 namespace cbs_sports_api.Models;
 
 [PrimaryKey(nameof(Id), nameof(Sport))]
-public record Player(int Id, string FirstName, string LastName, string Position, int? Age, SportEnum Sport);
+public record Player(int Id, string FirstName, string LastName, string Position, int? Age, SportEnum Sport)
+{
+	public string NameBrief {
+		get => Sport switch
+		{
+			SportEnum.Baseball => $"{InitialFromString(FirstName)}. {InitialFromString(LastName)}.",
+			SportEnum.Basketball => $"{FirstName} {InitialFromString(LastName)}.",
+			SportEnum.Football => $"{InitialFromString(FirstName)}. {LastName}",
+		};
+	}
+
+	public static string InitialFromString(string s) => s.Substring(0,1);
+}
