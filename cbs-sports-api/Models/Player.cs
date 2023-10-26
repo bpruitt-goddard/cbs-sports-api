fa462f268d590fb60942c9bf77c413f500b01472
Brian Pruitt-Goddard <bpruitt-goddard@github>
2023-10-25 21:45:53 -0700

    refactor: move name brief to dto response

--- cbs-sports-api/Models/Player.cs
+++ cbs-sports-api/Models/Player.cs
@@ -3,16 +3,4 @@ using Microsoft.EntityFrameworkCore;
 namespace cbs_sports_api.Models;
 
 [PrimaryKey(nameof(Id), nameof(Sport))]
-public record Player(int Id, string FirstName, string LastName, string Position, int? Age, SportEnum Sport)
-{
-	public string NameBrief {
-		get => Sport switch
-		{
-			SportEnum.Baseball => $"{InitialFromString(FirstName)}. {InitialFromString(LastName)}.",
-			SportEnum.Basketball => $"{FirstName} {InitialFromString(LastName)}.",
-			SportEnum.Football => $"{InitialFromString(FirstName)}. {LastName}",
-		};
-	}
-
-	public static string InitialFromString(string s) => s.Substring(0,1);
-}
+public record Player(int Id, string FirstName, string LastName, string Position, int? Age, SportEnum Sport);
--- cbs-sports-api/Models/PlayerGetDto.cs
+++ cbs-sports-api/Models/PlayerGetDto.cs
@@ -1,10 +1,20 @@
 namespace cbs_sports_api.Models;
 
 public record PlayerGetDto(int Id, string FirstName, string LastName, string Position, int? Age, SportEnum Sport, double AveragePositionAgeDiff)
-	: Player(Id, FirstName, LastName, Position, Age, Sport)
 {
 	public PlayerGetDto(Player player, double averagePositionAgeDiff)
 	  : this(player.Id, player.FirstName, player.LastName, player.Position, player.Age, player.Sport, averagePositionAgeDiff)
 	{
 	}
+
+	public string NameBrief {
+		get => Sport switch
+		{
+			SportEnum.Baseball => $"{InitialFromString(FirstName)}. {InitialFromString(LastName)}.",
+			SportEnum.Basketball => $"{FirstName} {InitialFromString(LastName)}.",
+			SportEnum.Football => $"{InitialFromString(FirstName)}. {LastName}",
+		};
+	}
+
+	public static string InitialFromString(string s) => s.Substring(0,1);
 }	
